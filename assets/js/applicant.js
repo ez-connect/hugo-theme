13e47bc9a7f7467debf77e720b53c052c8f77e49
Vinh <thanh.vinh@hotmail.com>
2021-05-11 22:37:26 +0700

    Apply job

--- assets/js/applicant.js
+++ assets/js/applicant.js
@@ -1,18 +1,25 @@
 // Submit CV
 document.addEventListener('DOMContentLoaded', function () {
-  const button = document.querySelector('#submitCV');
+  const button = document.querySelector('#applyJob');
   button.addEventListener('click', async function (e) {
     e.preventDefault();
+
+    const pathname = window.location.pathname;
+    const matches = pathname.match(/.*-(\d+)\/$/);
+    const job = matches[1];
 
     const form = document.querySelector('#applicantForm');
     const title = form.querySelector('input[name=title]').value;
     const phone = form.querySelector('input[name=phone]').value;
     const email = form.querySelector('input[name=email]').value;
-    const content = form.querySelector('textarea[name=content]').value;
+    // const content = form.querySelector('textarea[name=content]').value;
+    let content = document.querySelector('.ql-editor').innerHTML;
+    const turndownService = new TurndownService({ headingStyle: 'atx' });
+    content = turndownService.turndown(content);
 
-    const body = { title, phone, email, content };
+    const body = { title, phone, email, content, job };
 
-    console.log(body);
+    // console.log(body);
     const error = form.querySelector('.error');
     const baseURL = button.getAttribute('data-target');
     try {
@@ -23,21 +30,31 @@ document.addEventListener('DOMContentLoaded', function () {
       });
 
       const data = await res.json();
-      if (data.statusCode != 200) {
-        const messages = Object.entries(data.data.errors).map(function ([
-          k,
-          v,
-        ]) {
-          return `<li>${v}</li>`;
-        });
+      if (res.status == 200) {
+        error.classList.remove('error');
+        error.classList.add('success');
         error.innerHTML = `
-          <strong>${data.error}</strong>
-          <ul>
-            ${messages.join('')}
-          </ul>
+          Thanks for taking the time to apply for our position.
+          We appreciate your interest in our company.
+          We're currently in the process of taking applications for this position.
         `;
+        return;
       }
+
+      error.classList.remove('success');
+      error.classList.add('error');
+      const messages = Object.entries(data.data.errors).map(function ([k, v]) {
+        return `<li>${v}</li>`;
+      });
+      error.innerHTML = `
+        <strong>${data.error}</strong>
+        <ul>
+          ${messages.join('')}
+        </ul>
+      `;
     } catch (err) {
+      error.classList.remove('success');
+      error.classList.add('error');
       error.textContent = err;
     }
   });
